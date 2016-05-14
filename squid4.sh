91eb4acaa9ea75f0fcd068c8229286a3aad082a7
pujiermanto <pujiermanto@gmail.com>
2016-05-14 11:23:15 +0700

    siti ismayanti

--- squid4.sh
+++ squid4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p 'Masukan User Linux Anda: ' userver
-read -sp 'password Linux Anda: ' passvar
-echo Aku Yakin Kamu pasti bisa, selamat Menginstall yah $userver
+read -sp 'password Linux Anda: ' passserver
+echo Aku Yakin Kamu pasti bisa, selamat datang $userver di programku
 echo -n "Masukan Nama Anda :"
 read nama
 echo "Hello $nama"
