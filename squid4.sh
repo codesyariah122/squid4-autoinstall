57f28f5e5470aa60bc670be34ff081eadcb9861c
pujiermanto <pujiermanto@gmail.com>
2016-05-17 23:17:51 +0700

    siti ismayanti

--- squid4.sh
+++ squid4.sh
@@ -68,7 +68,15 @@ cp modules /etc/modules
 
 apt-get update && apt-get upgrade -y
 
-apt-get install devscripts build-essential openssl libssl-dev fakeroot libcppunit-dev pkg-config libsasl2-dev cdbs ebtables bridge-utils libcap2 libcap-dev libcap2-dev sysv-rc-conf iproute kernel-package libncurses5-dev fakeroot wget bzip2 debhelper linuxdoc-tools libselinux1-dev htop iftop dnstop perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python ccze pastebinit checkinstall libssl-dev htop iftop iptraf mtr-tiny bwm-ng ccze sysv-rc-conf devscripts build-essential openssl libssl-dev fakeroot libcppunit-dev libsasl2-dev cdbs ccze libfile-readbackwards-perl libcap2 libcap-dev libcap2-dev libnetfilter-conntrack-dev libfile-readbackwards-perl -y
+echo -e "anda telah selesai mengupdate server, \n"
+while true; do
+    read -p "Lanjuut?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "jawab aja yes atau no";;
+    esac
+done      
 
 tar -xvzf libsodium-0.7.0.tar.gz
 
@@ -96,6 +104,16 @@ chown unbound:root unbound_* && chmod 440 unbound_*
 killall unbound
 /etc/init.d/unbound stop
 /etc/init.d/unbound restart
+
+echo -e "selamat anda telah menginstall dns-crypt & unbound, \n"
+while true; do
+    read -p "Lanjuut dong yahh?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "jawab aja yes atau no";;
+    esac
+done  
 
 cd -
 tar -xzf libecap-1.0.0.tar.gz
@@ -196,12 +214,12 @@ echo "tail -f /var/log/squid/access.log | ccze"
 echo "buat direktory cache"
 squid -z
 echo "selesai_"
-echo -e "Instalasi Unbound dan Squid telah selesai. , \nsebaiknya restart server anda."
+echo -e "Selamat installasi dns-crypt+unbound & squid proxy version4 telah selesai di laksanakan. , \nHarap Restart dulu server anda."
 while true; do
-    read -p "Serius Lo mau Direstart?" yn
+    read -p "Silahkan Direstart bro?" yn
     case $yn in
         [Yy]* ) init 6; break;;
         [Nn]* ) exit;;
-        * ) echo "jawab aja yes atau no";;
+        * ) echo "jawab aja yes atau no gitu ajah ko repot";;
     esac
 done
